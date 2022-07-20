81ccf6795429c724a5a4039985da058a09622a9d
Fabianno Otávio <fabiannootavio08@gmail.com>
2022-07-20 16:24:37 -0300

    feat: add the finish funcionality

--- script.js
+++ script.js
@@ -12,7 +12,52 @@ function addsample(){
         console.log(list)
     }
 }
+function calc(){
+    main=document.querySelector('section')
+    max = maxNum(list)
+    min = minNum(list)
+    s = sum(list)
+    avg = average(list)
+    
+    main.innerHTML+=`There are ${list.length} numbers in the sample.<br>`
+    main.innerHTML+= `The biggest number of the sample is ${max}.<br>`
+    main.innerHTML+= `The smallest number of the sample is ${min}.<br>`
+    main.innerHTML+= `The sum of all the numbers in the sample is ${s}.<br>`
+    main.innerHTML+= `The arithmetic average of the sample is ${avg}.<br>`
+}
 
+function maxNum (x){
+    let max = 0
+    for(let i in x){
+        if(x[i]>max){
+            max=list[i]
+        }
+    }
+    return max
+}
+
+function minNum (x){
+    let min = maxNum(list)
+    for(let i in x){ 
+        if(x[i]<min){
+            min = list[i]
+        }
+    }
+    return min
+}
+
+function sum(x){
+    let s=0
+    for(let i in x){
+        s += x[i]
+    }
+    return s
+}
+
+function average(x){
+    avg = sum(x)/x.length
+    return avg
+}
 let list =[]
 
 
